d4809eec852d7f1ed0639287e7cdba5fd4f4e915
Emroze <emrozesd@users.noreply.github.com>
2018-12-03 22:56:13 -0800

    revisions 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,39 @@
 
-var Choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9; 
+var userChoices = [];
 
 document.onkeyup = function(event) {
 
   var userGuess = event.key;
-  var userChoices = []; 
-  userChoices.push(userGuess);
 
-  var computerGuess = Choices[Math.floor(Math.random() * Choices.length)];
+  var computerGuess = choices[Math.floor(Math.random() * choices.length)];
 
-    if (userChoices == computerGuess){
+  if (choices.indexOf(userGuess) > -1){
+
+    if (userGuess == computerGuess){
       wins++;
+      guessesLeft = 9;
+      userChoices = [];
     }
+
     else {
       guessesLeft--;
+      userChoices.push(userGuess);
+
     }
     if (guessesLeft == 0){
       losses++; 
+      guessesLeft = 9;
+      userChoices = []; 
     }
+  }
+  else {
+    alert("Pick a letter!");    
+  }
 
   document.getElementById("wins-text").textContent = wins;
   document.getElementById("losses-text").textContent = losses;
