71858def176c725edf94e1483dd23721522f5efa
Emroze <emrozesd@users.noreply.github.com>
2018-12-03 22:36:37 -0800

    revisions 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,31 @@
-//Document onkey event to listen for user input letter
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var userGuesses = [ ];
+
+var Choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 var losses = 0;
-var guesses = 9; 
-
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeft = document.getElementById("guesses-text");
-var userInputText = document.getElementById("user-input-text");
+var guessesLeft = 9; 
 
 document.onkeyup = function(event) {
 
-    var userGuesses = event.key;
+  var userGuess = event.key;
+  var userChoices = []; 
+  userChoices.push(userGuess);
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
+  var computerGuess = Choices[Math.floor(Math.random() * Choices.length)];
 
-    userInputText.textContent = userGuesses; 
+    if (userChoices == computerGuess){
+      wins++;
+    }
+    else {
+      guessesLeft--;
+    }
+    if (guessesLeft == 0){
+      losses++; 
+    }
 
-  };
-
-
-//With each input guess count -1
-
-
-//When Guess Count == 0 
-
-//If a variable in the user's array matches the computer's letter
-//Wins + 1
-
-//If none of the variables in the user's array matches the computer's letter 
-//Losses +1
-
-//Game resets
+  document.getElementById("wins-text").textContent = wins;
+  document.getElementById("losses-text").textContent = losses;
+  document.getElementById("guesses-text").textContent = guessesLeft;
+  document.getElementById("guesses-so-far").textContent = userChoices;
+  
+}
